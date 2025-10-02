08ac6ee3b271e648d7d12f589b8b1dc5b3ac0cfa
yukine049 <yukine049@example.com>
2025-10-02 15:10:09 +0900

    eight commit

--- book/management/commands/superuser.py
+++ book/management/commands/superuser.py
@@ -1,12 +1,12 @@
 from django.core.management.base import BaseCommand
-from djanfo.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model
 from django.conf import settings
 
 
 User = get_user_model()
 
 class Command(BaseCommand):
-    def handle(self,*args,**options):
+    def handle(self, *args, **options):
         if not User.objects.filter(username='your_name').exists():
             User.objects.create_superuser(
                 username='hibari',
